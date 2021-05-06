a80873a65861207da2da722fb4a38904ac6cb944
Bexx Modebadze <bexxmodd@seas.upenn.edu>
2021-05-06 09:45:37 -0400

    implemented struct

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 extern crate image;
 
-use std::collections::{BTreeMap, BinaryHeap};
-use image::{GenericImageView, Rgba, DynamicImage};
-use std::cmp::{Reverse, Eq, Ord};
+use std::collections::{BTreeMap, BinaryHeap, BTreeSet};
+use image::{GenericImageView, Rgba, DynamicImage, ImageBuffer};
+use std::cmp::{Reverse, Eq, Ord, Ordering};
 use std::hash::Hash;
+// use std::intrinsics::variant_count;
+use std::fmt;
 
 fn main() {
     let path = String::from("/home/bexx/Projects/paleatra/img/test.jpg");
@@ -16,32 +18,74 @@ fn main() {
 
     let mut i = 1;
     for x in top_ten {
-        println!("{}. {} - {}", i, x.0, x.1);
+        println!("{}. {} - {}", i, x.1.hex, x.0);
         i += 1;
     }
 
     // TODO: 1. create a new square image with each color frop top list and hex code
+    // let mut img = ImageBuffer::new(100, 100);
+    // for (x, y, pix) in img.enumerate_pixels_mut() {
+    //     *pix = image::Rgba([10, 10, 5, 255]);
+    // }
 
     // TODO: 2. append colored squares to the right if image is vertical, bottom if horizontal
 
     println!("Size of a map: {}", colors.len());
 }
 
+pub struct PalColor {
+    rgba: Rgba<u8>,
+    hex: String,
+    count: u32,
+}
+
+impl PalColor {
+    pub fn new(rgba: Rgba<u8>) -> PalColor {
+        PalColor {
+            rgba,
+            hex: generate_hex(&rgba),
+            count: 1
+        }
+    }
+
+    pub fn increment_count(&mut self) {
+        self.count += 1;
+    }
+}
+
+impl Ord for PalColor {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.hex.cmp(&other.hex)
+    }
+}
+
+impl PartialEq for PalColor {
+    fn eq(&self, other: &Self) -> bool {
+        self.hex == other.hex
+    }
+}
+
+impl PartialOrd for PalColor {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Eq for PalColor {}
+
+
 /// Grabs the n most frequently present elements from the Binary Tree Map
 ///
 /// # Arguments
-/// map - Which is a treemap where values are the count of key element
+/// set - Which is a tree set of PalColor struct
 /// n - how many most frequent entries to get
 ///
 /// # Return
-/// vector - of n most frequent values
-pub fn get_most_freq<K, V>(map: &BTreeMap<K, V>, n: usize) -> Vec<(&V, &K)>
-    where
-        K: Hash + Eq + Ord,
-        V: Eq + Ord {
+/// vector - of n most frequent PalColor structs
+pub fn get_most_freq(set: &BTreeSet<PalColor>, n: usize) -> Vec<(u32, &PalColor)> {
     let mut heap = BinaryHeap::with_capacity(n + 1);
-    for (x, count) in map.into_iter() {
-        heap.push(Reverse((count, x)));
+    for c in set.into_iter() {
+        heap.push(Reverse((c.count, c)));
         if heap.len() > n {
             heap.pop();
         }
@@ -56,14 +100,24 @@ pub fn get_most_freq<K, V>(map: &BTreeMap<K, V>, n: usize) -> Vec<(&V, &K)>
 /// * 'img' - A DynamicImage to decompose
 ///
 /// # Returns
-/// * Binary Tree Map with hex code of the color as key
-///          and count for that color as value
-pub fn get_colors_from(img: &DynamicImage) -> BTreeMap<String, i32> {
-    let mut colors = BTreeMap::new();
+/// * Binary Tree Set that contains PalColor structs
+pub fn get_colors_from(img: &DynamicImage) -> BTreeSet<PalColor> {
+    let mut colors : BTreeSet<PalColor> = BTreeSet::new();
 
     for i in img.pixels() {
-        *colors.entry(get_hex_of(&i.2))
-            .or_insert(0) += 1;
+        let c = PalColor::new(i.2);
+
+        if !colors.contains(&c) {
+            colors.insert(c);
+        } else {
+            match colors.take(&c) {
+                Some(mut v) => {
+                    v.increment_count();
+                    colors.insert(v);
+                },
+                None => {}
+            }
+        }
     }
     colors
 }
@@ -75,7 +129,7 @@ pub fn get_colors_from(img: &DynamicImage) -> BTreeMap<String, i32> {
 ///
 /// # Return
 /// hex code - of the color as a String
-pub fn get_hex_of(color: &Rgba<u8>) -> String {
+pub fn generate_hex(color: &Rgba<u8>) -> String {
     let mut hexcode = "x".to_owned();
     let red = format!("{:X}", color[0]);
     let green = format!("{:X}", color[1]);
