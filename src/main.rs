df46e28415ab1ef6ae8ba40bb0e03a34014592e9
Bexx Modebadze <bexxmodd@seas.upenn.edu>
2021-05-07 17:01:07 -0400

    reading cli commands

--- src/main.rs
+++ src/main.rs
@@ -7,27 +7,57 @@ extern crate image;
 use image::{GenericImageView};
 use crate::picture::FramedPicture;
 use crate::utils::{get_colors_from, get_most_freq};
+use structopt::StructOpt;
+use std::borrow::Cow;
+
+#[derive(StructOpt)]
+struct Cli {
+    #[structopt(parse(from_os_str))]
+    path: std::path::PathBuf,
+    #[structopt(parse(from_os_str))]
+    copy: std::path::PathBuf,
+}
+
+// impl Cli {
+//     fn basename(path: &str, sep: char) -> Cow<str> {
+//         let mut pieces = path.rsplit(sep);
+//         match pieces.next() {
+//             Some(p) => p.into(),
+//             None => path.into(),
+//         }
+//     }
+// }
 
 fn main() {
+    // TODO CLI args
+    // let args = Cli::from_args();
+
+
     let n = 10u32; // number of colors in palette
-    let path = String::from(
-        "/home/bexx/Projects/paleatra/img/ghost2.jpg");
-    let img1 = image::open(path).unwrap();
+    let img = image::open("img/rickmorty.jpg").unwrap();
 
-    println!("Original Dimensions: {}x{}", img1.dimensions().0, img1.dimensions().1);
-    let colors = get_colors_from(&img1);
+    println!("Original Dimensions: {}x{}", img.dimensions().0, img.dimensions().1);
+    let pb = indicatif::ProgressBar::new(100);
+    for i in 0..100 {
+        let colors = get_colors_from(&img);
 
 
-    let top_n = get_most_freq(&colors, n as usize);
+        let top_n = get_most_freq(&colors, n as usize);
 
-    let mut imgcpy = FramedPicture::new(
-        img1.width(), img1.height(), Some(n));
-    let palette = imgcpy.draw_palette(n as u32, &top_n);
-    imgcpy.copy_img_into(10, &img1);
-    imgcpy.stick_piece(&palette);
+        let mut imgcpy = FramedPicture::new(
+            img.width(), img.height(), Some(n));
+        let palette = imgcpy.draw_palette(n as u32, &top_n);
+        imgcpy.copy_img_into(10, &img);
+        imgcpy.stick_piece(&palette);
 
-    imgcpy.save_img("img/results/ghost2.jpg");
-    palette.save("img/results/ghost2_pal.png").unwrap();
+        pb.println(format!("[+] finished #{}", i));
+        pb.inc(1);
+    }
+    pb.finish_with_message("done");
+;
+
+    imgcpy.save_img("img/results/rickmorty.jpg");
+    palette.save("img/results/rickmorty_pal.png").unwrap();
     println!("Size of a map: {}", colors.len());
     println!("Framed image dimensions : {}x{}",
              imgcpy.get_dimensions().0, imgcpy.get_dimensions().1);
